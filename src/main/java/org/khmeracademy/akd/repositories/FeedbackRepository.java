96704515a3a919caba4b7c14c02e42c320c9f7d5
KimBunhong <bunhongkim93@gmail.com>
2016-08-13 14:32:58 +0700

    update Feedbacks repository by Dr Bunhong

--- src/main/java/org/khmeracademy/akd/repositories/FeedbackRepository.java
+++ src/main/java/org/khmeracademy/akd/repositories/FeedbackRepository.java
@@ -14,49 +14,44 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FeedbackRepository {
 	
-	//@Delete("DELETE FROM akd_documents WHERE doc_id=#{docID}")
+	@Delete(SQL.DELETE)
 	boolean delete(int id);
-	//@Update("UPDATE akd_documents SET title=#{title},des=#{des},embed_link=#{embedLink},thumbnail_url=#{thumbnailURL},export_link=#{exportLink},view=#{view},share=#{share},created_date=#{createdDate},doc_type_num=#{docTypeNum},user_id=#{userID},cat_id=#{catID},status=#{status} WHERE doc_id=#{docID}")
+	
+	@Update(SQL.UPDATE)
 	boolean update(Feedback feed);
 	
-	//@Insert("INSERT INTO akd_documents VALUES(#{docID},#{title},#{des},#{embedLink},#{thumbnailURL},#{exportLink},#{view},#{share},#{createdDate},#{docTypeNum},#{userID},#{catID},#{status})")
+	@Insert(SQL.INSERT)
 	boolean insert(Feedback feed);
 	
-	
-	//@Select("SELECT * from akd_documents")
+	@Select(SQL.SELECT)
 	@Results({
-		@Result(property="docID", column="doc_id"),
-		@Result(property="title", column="title"),
-		@Result(property="des", column="des"),
-		@Result(property="embedLink", column="embed_link"),
-		@Result(property="thumbnailURL", column="thumbnail_url"),
-		@Result(property="exportLink", column="export_link"),
-		@Result(property="view", column="view"),
-		@Result(property="share", column="share"),		
-		@Result(property="createdDate", column="created_date"),
-		@Result(property="docTypeNum", column="doc_type_num"),
-		@Result(property="userID", column="user_id"),
-		@Result(property="catID", column="cat_id"),
-		@Result(property="status", column="status")		
+		@Result(property="feedbackID", column="feed_id"),
+		@Result(property="date", column="feed_date"),
+		@Result(property="des", column="feed_des"),
+		@Result(property="status", column="status")
 	})
 	ArrayList<Feedback> findAll();
 	
-	//@Select("SELECT * from akd_documents WHERE doc_id=#{docID}")
+	@Select(SQL.FIND_ONE)
 	@Results({
-		@Result(property="docID", column="doc_id"),
-		@Result(property="title", column="title"),
-		@Result(property="des", column="des"),
-		@Result(property="embedLink", column="embed_link"),
-		@Result(property="thumbnailURL", column="thumbnail_url"),
-		@Result(property="exportLink", column="export_link"),
-		@Result(property="view", column="view"),
-		@Result(property="share", column="share"),		
-		@Result(property="createdDate", column="created_date"),
-		@Result(property="docTypeNum", column="doc_type_num"),
-		@Result(property="userID", column="user_id"),
-		@Result(property="catID", column="cat_id"),
-		@Result(property="status", column="status")	
+		@Result(property="feedbackID", column="feed_id"),
+		@Result(property="date", column="feed_date"),
+		@Result(property="des", column="feed_des"),
+		@Result(property="status", column="status")
 	})
 	Feedback findOne(int id);
 	
 }
+
+interface SQL{
+	String SELECT="SELECT * from akd_feedbacks";
+	String FIND_ONE="SELECT * from akd_feedbacks WHERE feed_id=#{feedbackID}";
+	String DELETE="DELETE FROM akd_feedbacks WHERE feed_id=#{feedbackID}";
+	String UPDATE="UPDATE akd_feedbacks SET feed_date=#{date},feed_des=#{des},status=#{status} WHERE feed_id=#{feedbackID}";
+	String INSERT="INSERT INTO akd_feedbacks(feed_id,feed_date,feed_des,status)VALUES(#{feedbackID},#{date},#{des},#{status})";
+	
+}
+
+
+
+
