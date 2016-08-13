b29a422fefe5ff66902ba9f2d3478f3751feacf7
KimBunhong <bunhongkim93@gmail.com>
2016-08-13 16:05:12 +0700

    userRepository commited by Dr.Wiliam bunhong

--- src/main/java/org/khmeracademy/akd/repositories/ReportRepository.java
+++ src/main/java/org/khmeracademy/akd/repositories/ReportRepository.java
@@ -14,50 +14,70 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository {
-	
-	//@Delete("DELETE FROM akd_documents WHERE doc_id=#{docID}")
+	@Delete(REPORT_SQL.DELETE)
 	boolean delete(int id);
-	//@Update("UPDATE akd_documents SET title=#{title},des=#{des},embed_link=#{embedLink},thumbnail_url=#{thumbnailURL},export_link=#{exportLink},view=#{view},share=#{share},created_date=#{createdDate},doc_type_num=#{docTypeNum},user_id=#{userID},cat_id=#{catID},status=#{status} WHERE doc_id=#{docID}")
+	
+	@Update(REPORT_SQL.UPDATE)
 	boolean update(Report rep);
 	
-	//@Insert("INSERT INTO akd_documents VALUES(#{docID},#{title},#{des},#{embedLink},#{thumbnailURL},#{exportLink},#{view},#{share},#{createdDate},#{docTypeNum},#{userID},#{catID},#{status})")
+	@Insert(REPORT_SQL.INSERT)
 	boolean insert(Report rep);
 	
 	
-	//@Select("SELECT * from akd_documents")
+	@Select(REPORT_SQL.SELECT)
 	@Results({
-		@Result(property="docID", column="doc_id"),
-		@Result(property="title", column="title"),
-		@Result(property="des", column="des"),
-		@Result(property="embedLink", column="embed_link"),
-		@Result(property="thumbnailURL", column="thumbnail_url"),
-		@Result(property="exportLink", column="export_link"),
-		@Result(property="view", column="view"),
-		@Result(property="share", column="share"),		
-		@Result(property="createdDate", column="created_date"),
-		@Result(property="docTypeNum", column="doc_type_num"),
+		@Result(property="reportID", column="report_id"),
+		@Result(property="date", column="created_date"),
+		@Result(property="remark", column="remark"),
 		@Result(property="userID", column="user_id"),
-		@Result(property="catID", column="cat_id"),
-		@Result(property="status", column="status")		
+		@Result(property="docID", column="doc_id"),
+		@Result(property="status", column="status")	
 	})
 	ArrayList<Report> findAll();
 	
-	//@Select("SELECT * from akd_documents WHERE doc_id=#{docID}")
+	@Select(REPORT_SQL.FIND_ONE)
 	@Results({
-		@Result(property="docID", column="doc_id"),
-		@Result(property="title", column="title"),
-		@Result(property="des", column="des"),
-		@Result(property="embedLink", column="embed_link"),
-		@Result(property="thumbnailURL", column="thumbnail_url"),
-		@Result(property="exportLink", column="export_link"),
-		@Result(property="view", column="view"),
-		@Result(property="share", column="share"),		
-		@Result(property="createdDate", column="created_date"),
-		@Result(property="docTypeNum", column="doc_type_num"),
+		@Result(property="reportID", column="report_id"),
+		@Result(property="date", column="created_date"),
+		@Result(property="remark", column="remark"),
 		@Result(property="userID", column="user_id"),
-		@Result(property="catID", column="cat_id"),
+		@Result(property="docID", column="doc_id"),
 		@Result(property="status", column="status")	
 	})
 	Report findOne(int id);
 	
 }
+
+interface REPORT_SQL{
+	String SELECT="SELECT * from akd_reports";
+	
+	String FIND_ONE="SELECT * from akd_reports WHERE report_id=#{reportID}";
+	
+	String DELETE="DELETE FROM akd_reports WHERE report_id=#{reportID}";
+	
+	String UPDATE="UPDATE akd_reports SET "
+			+ "created_date=#{date},"
+			+ "remark=#{remark},"
+			+ "user_id=#{userID},"
+			+ "doc_id=#{docID},"
+			+ "status=#{status} "
+			+ "WHERE report_id=#{reportID}";
+	
+	String INSERT="INSERT INTO "
+			+ "akd_reports(report_id,"
+			+ "created_date,"
+			+ "remark,"
+			+ "user_id,"
+			+ "doc_id,status)"
+			+ "VALUES"
+			+ "(#{reportID},"
+			+ "#{date},"
+			+ "#{remark},"
+			+ "#{userID},"
+			+ "#{docID},"
+			+ "#{status})";	
+}
+
+
+
+
